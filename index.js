73cb857bd92f8e3ada21f07d5b0911310c242261
Ahmed Danden <ahmeddanden2@gmail.com>
2021-10-21 10:28:27 +0100

    Ajouter les nouvelles fichiers(launch.json, index.js, package-lock.json)

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/signin', (req, res) => {
+    res.send('Sign in')
+  })
+  app.get('/signup', (req, res) => {
+    res.send('Sign up')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
