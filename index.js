8811054980bc2b06732cb6e253fcefbf78b2d29d
Ahmed Danden <ahmeddanden2@gmail.com>
2021-12-02 11:36:11 +0100

    on a faire une modification a indexjs pour faire la connection entre la base de donnee et handlebars, on a renommer tous les files html en hbs et on a faire l'appel de basse de donnee dans notre code html

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path');
-app.use(express.static('public'))
-var mysql = require('mysql');
-
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+const hbs = handlebars.create({
+  layoutsDir: __dirname + "/views/layouts",
+  extname: "hbs",
+});
+const port = 3000;
+const path = require("path");
+var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "canary"
+  database: "canary",
 });
 
-con.connect(function(err) {
-  if (err) throw err; 
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+con.connect(function (err) {
+  if (err) throw err;
   console.log("Connected!");
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+app.get("/", (req, res) => {
+  res.render("profile", {
+    layout: "index",
   });
-  app.get('/signin', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/signin.html'));
+});
+
+app.get("/signin", (req, res) => {
+  res.render("signin", {
+    layout: "index",
   });
-  app.get('/signup', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/signup.html'));
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup", {
+    layout: "index",
   });
-  app.get('/profile/:username', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/profile.html'));
-    console.log(req.params.username);
-    con.query("SELECT * FROM user where username = ?", req.params.username, function (err, result) {
+});
+app.get("/profile/:username", (req, res) => {
+  console.log(req.params.username);
+  con.query(
+    "SELECT * FROM user where username = ?",
+    req.params.username,
+    function (err, result) {
       if (err) throw err;
       console.log(result);
-    });
-  });
+      res.render("profile", {
+        layout: "index",
+        userinfo: result,
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
